06d6fe3df401c49b8ee1f551d5116aa7a5569a0a
Federico Agustin Barón <federicobaron@fb.com>
2020-07-07 21:39:53 -0400

    Added post details

--- app/src/main/java/com/example/instagramclone/PostsAdapter.java
+++ app/src/main/java/com/example/instagramclone/PostsAdapter.java
@@ -1,6 +1,8 @@
 package com.example.instagramclone;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +13,16 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.instagramclone.activities.PostDetailsActivity;
 import com.parse.ParseFile;
 
-import org.w3c.dom.Text;
+import org.parceler.Parcels;
 
 import java.util.List;
 
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
 
+    private static final String TAG = "PostsAdapter";
     private Context context;
     private List<Post> posts;
 
@@ -65,7 +69,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
     }
 
 
-    class ViewHolder extends RecyclerView.ViewHolder{
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         private TextView tvUsername;
         private ImageView ivImage;
@@ -77,6 +81,9 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
             tvUsername = itemView.findViewById(R.id.tvUsername);
             ivImage = itemView.findViewById(R.id.ivImage);
             tvDescription = itemView.findViewById(R.id.tvDescription);
+
+            // Add this as the itemView's OnClickListener
+            itemView.setOnClickListener(this);
         }
 
         public void bind(Post post) {
@@ -86,6 +93,28 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
             ParseFile image = post.getImage();
             if(image != null) {
                 Glide.with(context).load(post.getImage().getUrl()).into(ivImage);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            Log.i(TAG, "OnClick adapter");
+            // Gets item position
+            int position = getAdapterPosition();
+
+            // Make sure the position is valid i.e actually exists in the view
+            if(position != RecyclerView.NO_POSITION) {
+                // Get the post at the position, this won't work if the class is static
+                Post post = posts.get(position);
+
+                // Create intent for the new activity
+                Intent intent = new Intent(context, PostDetailsActivity.class);
+
+                // Serialize the post using the parceler, use its short name as a key
+                intent.putExtra(Post.class.getSimpleName(), Parcels.wrap(post));
+
+                // Show the activity
+                context.startActivity(intent);
             }
         }
     }
---
+++ app/src/main/java/com/example/instagramclone/activities/PostDetailsActivity.java
@@ -0,0 +1,42 @@
+package com.example.instagramclone.activities;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.instagramclone.Post;
+import com.example.instagramclone.R;
+import com.parse.ParseFile;
+
+import org.parceler.Parcels;
+
+public class PostDetailsActivity extends AppCompatActivity {
+
+    private Post post;
+    private TextView tvUsername;
+    private ImageView ivImage;
+    private TextView tvDescription;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_details);
+
+        tvUsername = findViewById(R.id.tvUsername);
+        ivImage = findViewById(R.id.ivImage);
+        tvDescription = findViewById(R.id.tvDescription);
+
+        // Unwrap the movie passed in via intent, using its simple name as a key
+        post = (Post) Parcels.unwrap(getIntent().getParcelableExtra(Post.class.getSimpleName()));
+
+        tvDescription.setText(post.getDescription());
+        tvUsername.setText(post.getUser().getUsername());
+        ParseFile image = post.getImage();
+        if(image != null) {
+            Glide.with(this).load(post.getImage().getUrl()).into(ivImage);
+        }
+    }
+}
