c2c1c02caec4d4856b030070660dd22690122e4f
Federico Agustin Barón <federicobaron@fb.com>
2020-07-08 19:30:51 -0400

    Added profile picture

--- app/src/main/java/com/example/instagramclone/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/instagramclone/fragments/SettingsFragment.java
@@ -1,7 +1,12 @@
 package com.example.instagramclone.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +18,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
@@ -23,7 +29,11 @@ import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.io.File;
+
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+import static android.app.Activity.RESULT_OK;
 
 public class SettingsFragment extends Fragment {
     private static final String TAG = "SettingsFragment";
@@ -37,6 +47,9 @@ public class SettingsFragment extends Fragment {
     private Button btnUpdateProfile;
     private Button btnChangePassword;
 
+    private File photoFile;
+    private String photoFileName = "photo.jpg";
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
     private static final String KEY_PROFILE_PIC = "profilePicture";
 //    private static final String KEY_USERNAME = "username";
 //    private static final String KEY_EMAIL = "email";
@@ -93,6 +106,14 @@ public class SettingsFragment extends Fragment {
 
         // Listens for logout button click
         logoutListener();
+
+        profilePicture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+
+        });
     }
 
     private void updateProfileListener() {
@@ -145,6 +166,64 @@ public class SettingsFragment extends Fragment {
                 Toast.makeText(getContext(), "Update successful", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.example.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                profilePicture.setImageBitmap(takenImage);
+                currentUser.put("profilePicture", new ParseFile(photoFile));
+                save();
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    private File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
     }
 
 }
